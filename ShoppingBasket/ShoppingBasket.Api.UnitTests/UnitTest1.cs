fcbf13717e611abc0108438e281111e200bd1500
sirlexis <alex.agyapong@hotmail.com>
2018-07-22 19:12:10 +0100

    Added project files for test and api

---
+++ ShoppingBasket/ShoppingBasket.Api.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ShoppingBasket.Api.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
